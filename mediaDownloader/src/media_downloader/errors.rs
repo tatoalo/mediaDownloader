65e914ae9da767fb58de6e14cbad072f79985299
tatoalo <apogliaghi@gmail.com>
2024-04-27 13:03:29 +0100

    chore: added more fine-grained errors

--- mediaDownloader/src/media_downloader/errors.rs
+++ mediaDownloader/src/media_downloader/errors.rs
@@ -6,6 +6,7 @@ use crate::{CHONK, CROSS_MARK, FAILED, MONKEY, RADIOACTIVE};
 
 #[derive(Debug)]
 pub enum MediaDownloaderError {
+    GenericError,
     UnsupportedDomain,
     BlobRetrievingError,
     DownloadError,
@@ -13,20 +14,27 @@ pub enum MediaDownloaderError {
     InvalidUrl,
     FileSizeExceeded,
     ImagesNotDownloaded,
-    IoErrorImagesDirectory(io::Error),
+    IoErrorDirectory(io::Error),
+    CustomParsingError(String),
+    ParsingError,
+    UnreachableResource,
+    DriverError,
 }
 
 impl Error for MediaDownloaderError {}
 
 impl From<io::Error> for MediaDownloaderError {
     fn from(error: io::Error) -> Self {
-        MediaDownloaderError::IoErrorImagesDirectory(error)
+        MediaDownloaderError::IoErrorDirectory(error)
     }
 }
 
 impl Display for MediaDownloaderError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            MediaDownloaderError::GenericError => {
+                write!(f, "{} Failed to download resource!", CROSS_MARK)
+            }
             MediaDownloaderError::UnsupportedDomain => {
                 write!(f, "{} Domain not supported!", MONKEY)
             }
@@ -48,9 +56,15 @@ impl Display for MediaDownloaderError {
             MediaDownloaderError::ImagesNotDownloaded => {
                 write!(f, "{} Images not downloaded, try again!", FAILED)
             }
-            MediaDownloaderError::IoErrorImagesDirectory(_) => {
+            MediaDownloaderError::IoErrorDirectory(_) => {
                 write!(f, "{} Error creating `images` directory!", MONKEY)
             }
+            MediaDownloaderError::CustomParsingError(_) => {
+                write!(f, "{}", self)
+            }
+            MediaDownloaderError::ParsingError => MediaDownloaderError::GenericError.fmt(f),
+            MediaDownloaderError::UnreachableResource => MediaDownloaderError::GenericError.fmt(f),
+            MediaDownloaderError::DriverError => MediaDownloaderError::GenericError.fmt(f),
         }
     }
 }
